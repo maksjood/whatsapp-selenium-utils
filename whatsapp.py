c77f1f63f7532b3a92d41bd6670ed155e7a7d156
javadhaqani <javad.haqani@gmail.com>
2022-12-11 00:35:32 +0330

    left out codes

--- whatsapp.py
+++ whatsapp.py
@@ -11,6 +11,5 @@ whatsapp = Whatsapp(os_path+'/chromedata')
 text = "Hey, this message was sent using Selenium"
 # whatsapp.send_message_to_chat(chat_name='Test', message=text) # TESTED
 # whatsapp.add_to_group(group_name='Test', contact_name='پوریا') # TESTED
-# whatsapp.send_message_to_chat(chat_name='Test', message='Hi, you were just added to this group using selenium.') # TESTED
-whatsapp.find_all_groups(search='dovinance')
+names = whatsapp.find_all_chats(search='dovinance')
 whatsapp.quit_driver()
